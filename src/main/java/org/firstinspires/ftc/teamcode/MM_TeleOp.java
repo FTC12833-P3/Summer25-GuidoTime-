69e1ad86edc18665a3cc92a40fade27b8d2691ac
FTC12833-P3 <ftc12833p3@gmail.com>
2025-04-03 20:29:20 -0400

    drive with sticks working

--- src/main/java/org/firstinspires/ftc/teamcode/MM_TeleOp.java
+++ src/main/java/org/firstinspires/ftc/teamcode/MM_TeleOp.java
@@ -6,7 +6,16 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 public class MM_TeleOp extends MM_OpMode{
     @Override
     public void runProcedures(){
-        multipleTelemetry.addLine("Helloooo");
-        multipleTelemetry.update();
+        while(opModeIsActive()) {
+            previousGamepad1.copy(currentGamepad1);
+            currentGamepad1.copy(gamepad1);
+
+            previousGamepad2.copy(currentGamepad2);
+            currentGamepad2.copy(gamepad2);
+
+            robot.drivetrain.driveWithSticks();
+            multipleTelemetry.addLine("Helloooo");
+            multipleTelemetry.update();
+        }
     }
 }
